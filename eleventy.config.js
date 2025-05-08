9eeac94286c97b3c7d2a0f873d1d50bf37259254
Harrel <marcin.slowik95@gmail.com>
2025-05-08 21:31:44 +0200

    display additional data

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,18 @@
 module.exports = cfg => {
-  cfg.addFilter("keys", obj => Object.keys(obj))
+  cfg.addFilter("prettyJson", obj => JSON.stringify(obj, null, 2))
+  cfg.addFilter("entries", obj => Object.entries(obj))
   cfg.addFilter('processBenchmarks', data => {
     const res = {}
     Object.entries(data).forEach(([provider, providerData]) => {
       Object.entries(providerData).forEach(([version, benchmarks]) => {
         benchmarks.forEach(benchmark => {
-          const keyword = benchmark.params.benchmarkFileName.split(/[/\\]/).pop().replace(/\.[^/.]+$/, "")
-          const keywordData = res[keyword] = res[keyword] ?? {}
+          const filename = benchmark.params.benchmarkFileName.split(/[/\\]/).pop().replace(/\.[^/.]+$/, "")
+          const keywordData = res[filename] = res[filename] ?? {}
+
+          keywordData.title = benchmark.params.model?.title
+          keywordData.description = benchmark.params.model?.description
+          keywordData.schema = benchmark.params.model?.schema
+          keywordData.instance = benchmark.params.model?.instance
 
           keywordData.scoreUnit = benchmark.primaryMetric.scoreUnit
           keywordData.versions = keywordData.versions ?? []
